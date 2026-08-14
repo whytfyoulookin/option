79104cc78d61099f6f5e9a4c3d30257d82d4633f
Dm Me Work <whythefuckyoulookin@ya.ru>
2026-08-14 13:12:44 +0300

    added tests and examples for new api

--- combining.go
+++ combining.go
@@ -1,33 +1,47 @@
 package option
 
-func And[T, U any](optA Option[T], optB Option[U]) Option[U] {
-	if optA.IsNone() {
+// And returns [None] if a contains no value. Otherwise, it returns b.
+//
+// b is evaluated before And is called. Use [AndThen] when the second Option
+// should be computed only if a contains a value.
+func And[T, U any](a Option[T], b Option[U]) Option[U] {
+	if a.IsNone() {
 		return None[U]()
 	}
 
-	return optB
+	return b
 }
 
-func Or[T any](optA, optB Option[T]) Option[T] {
-	if optA.IsSome() {
-		return optA
+// Or returns a if it contains a value. Otherwise, it returns b.
+//
+// b is evaluated before Or is called. Use [OrElse] when the fallback Option
+// should be computed only if a contains no value.
+func Or[T any](a, b Option[T]) Option[T] {
+	if a.IsSome() {
+		return a
 	}
 
-	return optB
+	return b
 }
 
-func Xor[T any](optA, optB Option[T]) Option[T] {
-	if optA.IsSome() == optB.IsSome() {
+// Xor returns a if only a contains a value, or b if only b contains a value.
+// It returns [None] otherwise.
+func Xor[T any](a, b Option[T]) Option[T] {
+	if a.IsSome() == b.IsSome() {
 		return None[T]()
 	}
 
-	if optB.IsNone() {
-		return optA
+	if b.IsNone() {
+		return a
 	}
 
-	return optB
+	return b
 }
 
+// AndThen returns [None] if opt contains no value. Otherwise, it calls f with
+// the contained value and returns the result.
+//
+// The function f is not called when opt contains no value.
 func AndThen[T, U any](opt Option[T], f func(T) Option[U]) Option[U] {
 	if opt.IsNone() {
 		return None[U]()
@@ -36,6 +50,10 @@ func AndThen[T, U any](opt Option[T], f func(T) Option[U]) Option[U] {
 	return f(opt.value)
 }
 
+// OrElse returns opt if it contains a value. Otherwise, it calls f and returns
+// the result.
+//
+// The function f is not called when opt contains a value.
 func OrElse[T any](opt Option[T], f func() Option[T]) Option[T] {
 	if opt.IsSome() {
 		return opt
--- doc.go
+++ doc.go
@@ -1,6 +1,11 @@
-// Package option provides generic Option type for representing values that may
-// be absent.
+// Package option provides a generic Option type for representing values that
+// may be absent.
 //
 // The zero value of [Option] contains no value and is equivalent to [None].
 // Use [Some] to construct an [Option] containing a value.
+//
+// Combining operations are package-level functions. [And] and [AndThen] may
+// change the contained type, and Go methods cannot declare additional type
+// parameters. [Or], [Xor], and [OrElse] use the same form to keep the combining
+// API consistent.
 package option
